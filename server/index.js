ab58d45baa375a3d8636b026bb9af889aa825fcf
Tsering Dingtsa <kesangdingtsa@gmail.com>
2022-08-04 21:08:55 -0700

    loaderio refactor route

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.get('/reviews/meta', getMeta);
 app.put('/reviews/:review_id/helpful', incrementHelpful);
 app.put('/reviews/:review_id/report', reportReview);
 app.post('/reviews', insertReview);
-app.get('loaderio-02253c2cf1dbe1a0c0f09707dcb30094.txt', loaderio);
+app.get('/loaderio-02253c2cf1dbe1a0c0f09707dcb30094.txt', loaderio);
 const port = process.env.Port || 3000;
 app.listen(port);
 console.log(`server listening at port: ${port}`)
